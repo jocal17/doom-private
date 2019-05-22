abe98fea72b9d90f3eeaed2ff5123f821e599b54
jocal17 <calivajoseph@gmail.com>
2019-05-22 16:25:56 -0400

    config

---
+++ README.md
@@ -0,0 +1,1 @@
+# My doom-emacs private config
---
+++ config.el
@@ -0,0 +1,25 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Joe Caliva"
+
+      doom-font (font-spec :family "Source Code Pro" :size 13) ;;mono narrow
+      doom-big-font (font-spec :family "Fira Mono" :size 19)
+      doom-theme 'doom-nord)
+
+;;;(set-frame-parameter nil 'fullscreen 'fullboth)
+;(evil-define-key 'visual evil-mc-key-map
+;  "A" #'evil-mc-make-cursor-in-visual-selection-end
+;  "I" #'evil-mc-make-cursor-in-visual-selection-beg)
+
+(setq debug-on-error t)
+
+(add-hook 'pdf-tools-enabled-hook 'pdf-view-midnight-minor-mode)
+(set-pretty-symbols! 'haskell-mode
+  :lambda "\\")
+
+(setq lsp-ui-doc-max-height 100)
+(setq org-log-done 'time
+      org-log-done 'note)
+
+(setq ccls-initialization-options `(:clang
+                                    (:extraArgs ["-isystem", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/"])))
